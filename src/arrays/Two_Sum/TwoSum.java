d91e009badda7c50f568ae0c04dd3c37c0273484
vedasole <vedasole001@gmail.com>
2023-09-25 00:31:17 +0530

    solved TwoSum problem

---
+++ src/arrays/Two_Sum/TwoSum.java
@@ -0,0 +1,32 @@
+package arrays.Two_Sum;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        for(int i=0;i<nums.length;i++){
+            int num1=nums[i];
+            for(int j=0;j<nums.length;j++){
+                if(j!=i){
+                    int num2=nums[j];
+                    if((num1+num2)==target){
+                        int [] index={i,j};
+                        return index;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int [] nums={2,7,11,15};
+        int target=9;
+        System.out.println("target : " + target);
+
+        TwoSum twoSum=new TwoSum();
+        int [] index=twoSum.twoSum(nums,target);
+        for (int j : index) {
+            System.out.println(j + " : " + nums[j]);
+        }
+    }
+}
