c7111b10942e320659d80d6ea1dcd9c37cbab85e
vedasole <vedasole001@gmail.com>
2023-09-25 19:35:19 +0530

    solved palindrome numbers problem

---
+++ src/common/PalindromeNumber.java
@@ -0,0 +1,20 @@
+package common;
+
+public class PalindromeNumber {
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0))return false;
+        int rev=0;
+        while(x>rev) {
+            rev = rev*10+(x%10);
+            x/=10;
+        }
+        return (x==rev) || (x==rev/10);
+    }
+
+    public static void main(String[] args) {
+        int num = 393;
+        boolean isPalindrome = isPalindrome(num);
+        System.out.println("The number is a palindrome?\n" + isPalindrome);
+    }
+
+}
