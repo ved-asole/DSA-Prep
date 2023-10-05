c8676f064b1d1609df2fe665313ead7363b8d2f2
Ved Asole <vedasole001@gmail.com>
2023-10-05 22:25:13 +0530

    solved reverseArray problem

---
+++ src/arrays/reverseArray/ReverseArray.java
@@ -0,0 +1,38 @@
+package arrays.reverseArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReverseArray {
+
+    public static String reverseWord(String str)
+    {
+        char [] charArray = str.toCharArray();
+        int length = charArray.length;
+        char temp;
+
+        for(int i=0; i<length/2; i++){
+
+            temp = charArray[i];
+            charArray[i] = charArray[length-i-1];
+            charArray[length-i-1] = temp;
+
+        }
+
+        return new String(charArray);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while(t-- >0)
+        {
+            String str = read.readLine();
+            System.out.println(reverseWord(str));
+        }
+    }
+
+}
