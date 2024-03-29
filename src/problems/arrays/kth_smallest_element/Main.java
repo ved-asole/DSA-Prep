d50c26f349e59b1f7de8fe0e5b849f24c5edef49
vedasole <vedasole001@gmail.com>
2024-03-29 23:44:46 +0530

    added Kth smallest element in array problem

---
+++ src/problems/arrays/kth_smallest_element/Main.java
@@ -0,0 +1,28 @@
+package problems.arrays.kth_smallest_element;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int t=sc.nextInt();
+
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+
+            int arr[]=new int[n];
+
+            for(int i=0;i<n;i++)
+                arr[i]=sc.nextInt();
+
+            int k=sc.nextInt();
+            Solution ob = new Solution();
+            out.println(ob.kthSmallest(arr, 0, n-1, k));
+        }
+        out.flush();
+    }
+}
---
+++ src/problems/arrays/kth_smallest_element/Solution.java
@@ -0,0 +1,14 @@
+package problems.arrays.kth_smallest_element;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static int kthSmallest(int[] arr, int l, int r, int k)
+    {
+        //Time complexity: O(N logN) and space complexity: O(1)
+        Arrays.sort(arr);
+        return arr[k-1];
+    }
+
+}
