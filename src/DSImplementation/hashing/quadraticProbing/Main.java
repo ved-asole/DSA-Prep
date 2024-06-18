94d5c1e1fd22239f202d6e6d27165b9abfbb4c77
vedasole <vedasole001@gmail.com>
2024-06-18 23:12:29 +0530

    added hashing-quadraticProbing implementation

---
+++ src/DSImplementation/hashing/quadraticProbing/Main.java
@@ -0,0 +1,16 @@
+package DSImplementation.hashing.quadraticProbing;
+
+public class Main {
+
+    public static void main(String[] args) {
+        QuadraticProbing quadraticProbing = new QuadraticProbing(13);
+        quadraticProbing.insertKeyInHashTable("The");
+        quadraticProbing.insertKeyInHashTable("quick");
+        quadraticProbing.insertKeyInHashTable("brown");
+        quadraticProbing.insertKeyInHashTable("fox");
+        quadraticProbing.insertKeyInHashTable("over");
+        quadraticProbing.insertKeyInHashTable("lazy");
+        quadraticProbing.displayHashTable();
+    }
+
+}
