7d181e7b249076051628ab8611ce0690ef8b09ab
vedasole <vedasole001@gmail.com>
2024-06-28 23:30:50 +0530

    added searching-linearSearch implementation

---
+++ src/searching/linearSearch/LinearSearch.java
@@ -0,0 +1,16 @@
+package searching.linearSearch;
+
+public class LinearSearch {
+
+    public int linearSearch(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                System.out.println("The element is found at the index : " + i);
+                return i;
+            }
+        }
+        System.out.println("The element" + value + " is not found");
+        return -1;
+    }
+
+}
---
+++ src/searching/linearSearch/Main.java
@@ -0,0 +1,14 @@
+package searching.linearSearch;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 3, 2, 10, 23, 11};
+
+        LinearSearch linearSearch = new LinearSearch();
+        linearSearch.linearSearch(arr, 10);
+
+    }
+
+}
