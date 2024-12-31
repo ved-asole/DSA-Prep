444f3e523637b6e9b9033c2d4c22ad42d423bce3
vedasole <vedasole001@gmail.com>
2024-12-31 23:08:42 +0530

    refactor(binarySearch): comment out system output logs

--- src/searching/binarySearch/BinarySearch.java
+++ src/searching/binarySearch/BinarySearch.java
@@ -6,7 +6,7 @@ public class BinarySearch {
         int start = 0;
         int end = arr.length - 1;
         int mid = (start+end)/2;
-        System.out.printf("Start: %d, Mid: %d, End: %d\n", start, mid, end);
+//        System.out.printf("Start: %d, Mid: %d, End: %d\n", start, mid, end);
         while (start <= end) {
             if (arr[mid] == value) {
                 System.out.println("The element is found at the index : " + mid);
@@ -17,7 +17,7 @@ public class BinarySearch {
                 end = mid - 1;
             }
             mid = (start+end)/2;
-            System.out.printf("Start: %d, Mid: %d, End: %d\n", start, mid, end);
+//            System.out.printf("Start: %d, Mid: %d, End: %d\n", start, mid, end);
         }
         System.out.println("The element " + value + " is not found");
         return -1;
